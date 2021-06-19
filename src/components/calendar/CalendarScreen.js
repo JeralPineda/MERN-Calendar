8526b8a39ab43c9752eeec8f5098134e9e0c0342
Jeral <jeral.cris@gmail.com>
2021-06-19 15:09:15 -0600

    Cambiaando el estilo de los eventos

--- src/components/calendar/CalendarScreen.js
+++ src/components/calendar/CalendarScreen.js
@@ -18,15 +18,29 @@ const events = [
       start: moment().toDate(), //new Date
       end: moment().add(2, 'hours').toDate(),
       bgcolor: '#fafafa',
+      notes: 'Comprar pastel',
    },
 ];
 
 export const CalendarScreen = () => {
+   const eventStyleGetter = (event, start, end, isSelected) => {
+      const style = {
+         backgroundColor: '#367CF7',
+         borderRadius: '0px',
+         opacity: 0.8,
+         display: 'block',
+         color: 'white',
+      };
+
+      return {
+         style,
+      };
+   };
    return (
       <div className='calendar-screen'>
          <Navbar />
 
-         <Calendar localizer={localizer} events={events} startAccessor='start' endAccessor='end' messages={messages} />
+         <Calendar localizer={localizer} events={events} startAccessor='start' endAccessor='end' messages={messages} eventPropGetter={eventStyleGetter} />
       </div>
    );
 };
