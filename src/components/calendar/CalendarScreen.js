ebf398137417255aaabd870bd5192a0a67e37682
Jeral <jeral.cris@gmail.com>
2021-06-19 15:46:00 -0600

    Personalizar el cuadro y disparar eventos del calendario

--- src/components/calendar/CalendarScreen.js
+++ src/components/calendar/CalendarScreen.js
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Calendar, momentLocalizer } from 'react-big-calendar';
 import moment from 'moment';
 
 import { Navbar } from '../ui/Navbar';
 import { messages } from '../../helpers/calendar-messages-es';
+import { CalendarEvent } from './CalendarEvent';
 
 import 'react-big-calendar/lib/css/react-big-calendar.css';
 import 'moment/locale/es'; //Cambiar idioma al calendario(fechas)
@@ -19,10 +20,30 @@ const events = [
       end: moment().add(2, 'hours').toDate(),
       bgcolor: '#fafafa',
       notes: 'Comprar pastel',
+      user: {
+         _id: '1234',
+         name: 'Jeral',
+      },
    },
 ];
 
 export const CalendarScreen = () => {
+   const [lastView, setLastView] = useState(localStorage.getItem('lastView') || 'month');
+
+   const onDoubleClick = (e) => {
+      console.log(e);
+   };
+
+   const onSelectEvent = (e) => {
+      console.log(e);
+   };
+
+   const onViewChange = (e) => {
+      //al momento de hacer un cambio, guardar y decirle al calendario que inicie desde ahi
+      setLastView(e);
+      localStorage.setItem('lastView', e);
+   };
+
    const eventStyleGetter = (event, start, end, isSelected) => {
       const style = {
          backgroundColor: '#367CF7',
@@ -36,11 +57,26 @@ export const CalendarScreen = () => {
          style,
       };
    };
+
    return (
       <div className='calendar-screen'>
          <Navbar />
 
-         <Calendar localizer={localizer} events={events} startAccessor='start' endAccessor='end' messages={messages} eventPropGetter={eventStyleGetter} />
+         <Calendar
+            localizer={localizer}
+            events={events}
+            startAccessor='start'
+            endAccessor='end'
+            messages={messages} //mensajes en español
+            eventPropGetter={eventStyleGetter} //estilos del evento
+            onDoubleClickEvent={onDoubleClick}
+            onSelectEvent={onSelectEvent}
+            onView={onViewChange}
+            view={lastView}
+            components={{
+               event: CalendarEvent,
+            }}
+         />
       </div>
    );
 };
