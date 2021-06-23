4d02767e4469824a5162f5164cf1e6c91c51169f
Jeral <jeral.cris@gmail.com>
2021-06-22 19:58:49 -0600

    Guardando el token y fecha de creacion del token en el localstorage

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -2,10 +2,15 @@ import { fetchSinToken } from '../helpers/fetch';
 
 export const startLogin = (email, password) => {
    return async () => {
+      //    Petición login
       const resp = await fetchSinToken('auth', { email, password }, 'POST');
 
       const body = await resp.json();
 
-      console.log(body);
+      //   Si el body es ok: true guardamos el token en localstorage
+      if (body.ok) {
+         localStorage.setItem('x-token', body.token);
+         localStorage.setItem('token-init-date', new Date().getTime()); //fecha en que se creo el token
+      }
    };
 };
