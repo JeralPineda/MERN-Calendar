21347e62b3a86008c595185d47d9e58ec7ac5b44
Jeral <jeral.cris@gmail.com>
2021-06-22 21:42:44 -0600

    Creacion de un nuevo usuario

--- src/components/auth/LoginScreen.js
+++ src/components/auth/LoginScreen.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import { useDispatch } from 'react-redux';
+import Swal from 'sweetalert2';
 
 import { useForm } from '../../hooks/useForm';
-import { startLogin } from '../../actions/auth';
+import { startLogin, startRegister } from '../../actions/auth';
 
 import './login.css';
 
@@ -13,8 +14,15 @@ export const LoginScreen = () => {
       lEmail: 'jeral@gmail.com',
       lPassword: '123456',
    });
-
    const { lEmail, lPassword } = formLoginValues;
+
+   const [formRegisterValues, handleRegisterInputChange] = useForm({
+      rName: 'Cristopher',
+      rEmail: 'Cris@gmail.com',
+      rPassword1: '123456',
+      rPassword2: '123456',
+   });
+   const { rName, rEmail, rPassword1, rPassword2 } = formRegisterValues;
 
    //  Effecto en los formularios de login y registro
    const toggleForm = (e) => {
@@ -27,6 +35,17 @@ export const LoginScreen = () => {
       e.preventDefault();
 
       dispatch(startLogin(lEmail, lPassword));
+   };
+
+   const handleRegister = (e) => {
+      e.preventDefault();
+
+      //   Validación de las contraseñas
+      if (rPassword1 !== rPassword2) {
+         Swal.fire('Error', 'Ambas contraseñas deben de ser iguales', 'error');
+      }
+
+      dispatch(startRegister(rEmail, rPassword1, rName));
    };
 
    return (
@@ -71,18 +90,46 @@ export const LoginScreen = () => {
 
             <div className='user signupBx'>
                <div className='formBx'>
-                  <form>
+                  <form onSubmit={handleRegister}>
                      <h2>Registro</h2>
 
-                     <input type='text' name='' placeholder='Nombre' />
+                     <input
+                        type='text' //
+                        placeholder='Nombre'
+                        name='rName'
+                        value={rName}
+                        onChange={handleRegisterInputChange}
+                     />
 
-                     <input type='email' name='' placeholder='Correo' />
+                     <input
+                        type='email' //
+                        placeholder='Correo'
+                        name='rEmail'
+                        value={rEmail}
+                        onChange={handleRegisterInputChange}
+                     />
 
-                     <input type='password' name='' placeholder='Contraseña' />
+                     <input
+                        type='password' //
+                        placeholder='Contraseña'
+                        name='rPassword1'
+                        value={rPassword1}
+                        onChange={handleRegisterInputChange}
+                     />
 
-                     <input type='password' name='' placeholder='Confirmar Contraseña' />
+                     <input
+                        type='password' //
+                        placeholder='Confirmar Contraseña'
+                        name='rPassword2'
+                        value={rPassword2}
+                        onChange={handleRegisterInputChange}
+                     />
 
-                     <input type='submit' name='' value='Crear cuenta' />
+                     <input
+                        type='submit' //
+                        name=''
+                        value='Crear cuenta'
+                     />
 
                      <p className='signup'>
                         Ya tienes una cuenta ?
