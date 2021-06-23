2896bb69b4ee232d38744a84eec8b43f094ae3dd
Jeral <jeral.cris@gmail.com>
2021-06-22 22:52:18 -0600

    chequeando que se halla realizado la autenticacion y mostrar un loader

--- src/router/AppRouter.js
+++ src/router/AppRouter.js
@@ -1,17 +1,24 @@
 import React, { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
 
 import { startChecking } from '../actions/auth';
 import { LoginScreen } from '../components/auth/LoginScreen';
 import { CalendarScreen } from '../components/calendar/CalendarScreen';
+import { Loader } from '../components/loader/Loader';
 
 export const AppRouter = () => {
+   const { checking } = useSelector((state) => state.auth);
+
    const dispatch = useDispatch();
 
    useEffect(() => {
       dispatch(startChecking());
    }, [dispatch]);
+
+   if (checking) {
+      return <Loader />;
+   }
 
    return (
       <Router>
