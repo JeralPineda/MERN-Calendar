f3f467b145b723b808d42b55bf9032be9d3b6a18
Jeral <jeral.cris@gmail.com>
2021-06-22 20:37:37 -0600

    peticion para revalidar el token y generar uno nuevo

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -1,10 +1,32 @@
-import { fetchSinToken } from '../helpers/fetch';
+import { fetchSinToken, fetchConToken } from '../helpers/fetch';
 import { types } from '../types/types';
 
 export const startLogin = (email, password) => {
    return async (dispatch) => {
       //    Petición login
       const resp = await fetchSinToken('auth', { email, password }, 'POST');
+
+      const body = await resp.json();
+
+      //   Si el body es ok: true guardamos el token en localstorage
+      if (body.ok) {
+         localStorage.setItem('x-token', body.token);
+         localStorage.setItem('token-init-date', new Date().getTime()); //fecha en que se creo el token
+
+         dispatch(
+            login({
+               uid: body.uid,
+               name: body.name,
+            })
+         );
+      }
+   };
+};
+
+export const startChecking = () => {
+   return async (dispatch) => {
+      //    Petición login
+      const resp = await fetchConToken('renew');
 
       const body = await resp.json();
 
