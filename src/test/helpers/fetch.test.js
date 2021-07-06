e9f6ad22c72ce6b644afc8ed717560f1f705c062
Jeral <jeral.cris@gmail.com>
2021-07-06 09:53:46 -0600

    Pruebas en helpers FetchConToken

--- src/test/helpers/fetch.test.js
+++ src/test/helpers/fetch.test.js
@@ -1,6 +1,8 @@
-import { fetchSinToken } from '../../helpers/fetch';
+import { fetchConToken, fetchSinToken } from '../../helpers/fetch';
 
 describe('Pruebas en el helper Fetch', () => {
+   let token = '';
+
    test('fetch sin token debe de funcionar', async () => {
       const resp = await fetchSinToken('auth', { email: 'jeral@gmail.com', password: '123456' }, 'POST');
 
@@ -8,5 +10,17 @@ describe('Pruebas en el helper Fetch', () => {
 
       const body = await resp.json();
       expect(body.ok).toBe(true);
+
+      token = body.token;
+   });
+
+   test('fetch con token debe de funcionar', async () => {
+      localStorage.setItem('x-token', token);
+
+      const resp = await fetchConToken('events/60d193b592d92a0015c925c8', {}, 'DELETE');
+
+      const body = await resp.json();
+
+      expect(body.msg).toBe('Evento no existe por ese id');
    });
 });
