d4fa5cc86bf42db2bb70bce21c83c67b34b5187f
Jeral <jeral.cris@gmail.com>
2021-07-12 15:43:22 -0600

    Prueba para validar que las fechas no sean iguales

--- src/test/components/calendar/CalendarModal.test.js
+++ src/test/components/calendar/CalendarModal.test.js
@@ -6,16 +6,17 @@ import configureStore from 'redux-mock-store';
 import thunk from 'redux-thunk';
 import moment from 'moment';
 
+import { act } from '@testing-library/react';
+import Swal from 'sweetalert2';
+
 import '@testing-library/jest-dom';
 import { CalendarModal } from '../../../components/calendar/CalendarModal';
 import { eventStartUpdated, eventClearActiveEvent, eventStartAddNew } from '../../../actions/events';
 
-// import { act } from '@testing-library/react';
-// import Swal from 'sweetalert2';
+jest.mock('sweetalert2', () => ({
+   fire: jest.fn(),
+}));
 
-// jest.mock('sweetalert2', () => ({
-//    fire: jest.fn(),
-// }));
 jest.mock('../../../actions/events', () => ({
    eventStartUpdated: jest.fn(),
    eventClearActiveEvent: jest.fn(),
@@ -126,5 +127,26 @@ describe('Pruebas en <CalendarModal />', () => {
       });
 
       expect(eventStartAddNew).toHaveBeenCalled();
+   });
+
+   test('debe de validar las fechas', () => {
+      wrapper.find('input[name="title"]').simulate('change', {
+         target: {
+            name: 'title',
+            value: 'Hola pruebas',
+         },
+      });
+
+      const hoy = new Date();
+
+      act(() => {
+         wrapper.find('DateTimePicker').at(1).prop('onChange')(hoy);
+      });
+
+      wrapper.find('form').simulate('submit', {
+         preventDefault() {},
+      });
+
+      expect(Swal.fire).toHaveBeenCalledWith('Error', 'La fecha fin debe de ser mayor a la fecha de inicio', 'error');
    });
 });
